b230dcd77e88abeedaa4f23c110dd104dba2b673
steffi.adhikary <stefs0611@gmail.com>
2020-07-30 16:49:58 +0530

    validation: checking of empty field

--- registration.js
+++ registration.js
@@ -1,21 +1,36 @@
 function checkIfValuesAreEmpty(name, email, password, rePassword){
-    if (name === '' || typeof name === 'undefined' || email === '' || typeof email === 'undefined' || password === '' || typeof password === 'undefined' || rePassword === '' || typeof rePassword === 'undefined'){
-      return false;
+    
+    if (name === ''){
+        document.getElementById("error-name").style.display = "inline";
+    }else{
+        document.getElementById("error-name").style.display = "none";
     }
-    else{
-      return true;
+
+    if (email === ''){
+        document.getElementById("error-email").style.display = "inline";
+    }else{
+        document.getElementById("error-email").style.display = "none";
     }
+
+    if (password === ''){
+        document.getElementById("error-psw").style.display = "inline";
+    }else{
+        document.getElementById("error-psw").style.display = "none";
+    }
+
+    if (rePassword === ''){
+        document.getElementById("error-repsw").style.display = "inline";
+    }else{
+        document.getElementById("error-repsw").style.display = "none";
+    }
+
   }
   function validateData(){
       var name = document.getElementById("name").value;
       var email = document.getElementById("emailid").value;
       var password = document.getElementById("pswd").value;
       var rePassword = document.getElementById("repswd").value;
-      var checkVal = checkIfValuesAreEmpty(name, email, password, rePassword);
-    console.log("the value is:", checkVal);
-    if( checkVal == false){
-      document.getElementById("error-div").style.display = "inline";
-    }
+      checkIfValuesAreEmpty(name, email, password, rePassword);
   }
   
   document.addEventListener("DOMContentLoaded", function(){
